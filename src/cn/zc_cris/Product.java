06601b94378f3ad5e92325fff34a17d007f18ec3
zc-cris <990435014@qq.com>
2018-02-17 17:01:37 +0800

    hello struts2！

---
+++ src/cn/zc_cris/Product.java
@@ -0,0 +1,44 @@
+package cn.zc_cris;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+	private Integer id;
+	private String name;
+	private String desc;
+	private BigDecimal price;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", desc=" + desc + ", price=" + price + "]";
+	}
+	
+	public String save() {
+		System.out.println("save: "+this);
+		return "details";
+	}
+}
